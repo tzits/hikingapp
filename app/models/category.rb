8ec97b26b5f32e41cec78ae0f5eb391516b4edb3
tzits <tzitsman@gmail.com>
2016-11-18 14:48:49 -0800

    creates join table between list and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :items
+  has_many :list_contents, dependent: :destroy
+  has_many :lists, through: :list_contents
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118221533) do
+ActiveRecord::Schema.define(version: 20161118223448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,5 +34,22 @@ ActiveRecord::Schema.define(version: 20161118221533) do
     t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
 
+  create_table "list_contents", force: :cascade do |t|
+    t.integer  "list_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_list_contents_on_category_id", using: :btree
+    t.index ["list_id"], name: "index_list_contents_on_list_id", using: :btree
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "items", "categories"
+  add_foreign_key "list_contents", "categories"
+  add_foreign_key "list_contents", "lists"
 end
