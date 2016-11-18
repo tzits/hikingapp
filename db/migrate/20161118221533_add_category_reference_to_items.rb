dbbaad1492e864cae5eda7709668d09c2549e548
tzits <tzitsman@gmail.com>
2016-11-18 14:21:04 -0800

    items and categories are correctly linkd

---
+++ db/migrate/20161118221533_add_category_reference_to_items.rb
@@ -0,0 +1,5 @@
+class AddCategoryReferenceToItems < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :items, :category, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118221301) do
+ActiveRecord::Schema.define(version: 20161118221533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,11 @@ ActiveRecord::Schema.define(version: 20161118221301) do
     t.float    "size"
     t.string   "unit"
     t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
 
+  add_foreign_key "items", "categories"
 end
