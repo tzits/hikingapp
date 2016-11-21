03e10d26b41a17d42e6ccfbe502111e83ffd5183
tzits <tzitsman@gmail.com>
2016-11-21 12:02:59 -0800

    allows user to add items

--- app/controllers/user_list_category_items_controller.rb
+++ app/controllers/user_list_category_items_controller.rb
@@ -1,2 +1,16 @@
 class UserListCategoryItemsController < ApplicationController
+
+  def create
+    ulci = UserListCategoryItem.new(ulci_params)
+    if ulci.valid?
+      ulci.save
+      redirect_to user_path(User.find(1))
+    else
+      redirect_to :back
+    end
+  end
+  private
+  def ulci_params
+    params.require(:user_list_category_item).permit(:list_id, :category_id, :user_id, :item_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :users
   get '/lists', to: 'lists#index'
   get '/lists/:id', to: 'lists#show', as: 'list'
-  post '/user_list_category_items', to: 'user_list_category_items#new'
+  post '/user_list_category_items', to: 'user_list_category_items#create'
   get '/items/:id', to: 'items#show', as: 'item'
 end
