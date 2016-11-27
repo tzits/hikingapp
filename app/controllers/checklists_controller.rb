3114de1040e95e8862d13ac0181b88dc5a480752
tzits <tzitsman@gmail.com>
2016-11-26 16:35:03 -0800

    adds a ton of stuff

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -1,4 +1,8 @@
 class ChecklistsController < ApplicationController
+  def index
+    @checklsits = Checklist.all
+  end
   def show
+    @checklist = Checklist.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
-    explode
+
     if @user.valid?
 			@user.save
       redirect_to user_path(@user)
