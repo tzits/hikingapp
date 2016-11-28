0443d4dca2e7733f5e41b3ca30c4a1c6ee463eb0
tzits <tzitsman@gmail.com>
2016-11-28 07:43:29 -0800

    adds cumulative weight

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,11 @@
 module CategoriesHelper
+
+  def find_weight(user)
+    items = Item.find_by_sql("SELECT * FROM user_list_category_items WHERE user_id = #{current_user.id} AND list_id = #{params[:id]}")
+    w = 0
+    items.each do |i|
+      w += Item.find(i).weight
+    end
+    w
+  end
 end
