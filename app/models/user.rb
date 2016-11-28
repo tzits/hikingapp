21cfc6553a086085751550fe7b41aab9b7a7babc
tzits <tzitsman@gmail.com>
2016-11-28 14:36:01 -0800

    checklist to modal and more seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :comments
   has_many :user_list_category_items, dependent: :destroy
   has_many :items, through: :user_list_category_items
-  has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "https://crazyhikers.files.wordpress.com/2016/05/img_0880.jpg"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://crazyhikers.files.wordpress.com/2016/05/img_0880.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   validates :first_name, :last_name, length: { minimum: 2, maximum: 63 }, presence: true
   validates :email, uniqueness: true, presence: true,
