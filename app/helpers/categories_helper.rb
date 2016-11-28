adb268604b987a59de50544055ed11087ad1ed75
tzits <tzitsman@gmail.com>
2016-11-28 15:55:04 -0800

    adds and sorts checklists, some user styling

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,10 +1,12 @@
 module CategoriesHelper
 
   def find_weight(user)
-    items = Item.find_by_sql("SELECT * FROM user_list_category_items WHERE user_id = #{user.id} AND list_id = #{params[:id]}")
     w = 0
-    items.each do |i|
-      w += (Item.find(i.item_id).weight)
+    if current_user
+      items = Item.find_by_sql("SELECT * FROM user_list_category_items WHERE user_id = #{user.id} AND list_id = #{params[:id]}")
+      items.each do |i|
+        w += (Item.find(i.item_id).weight)
+      end
     end
     w
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128011315) do
+ActiveRecord::Schema.define(version: 20161128223659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20161128011315) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "advice"
+    t.boolean  "important"
     t.index ["checklist_id"], name: "index_checklist_items_on_checklist_id", using: :btree
   end
 
