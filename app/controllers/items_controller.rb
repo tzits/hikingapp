c98487e3bf7df51ef772436c22be5a49f13c5884
tzits <tzitsman@gmail.com>
2016-11-29 12:04:52 -0800

    new item redirects correctly

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,11 @@
 class ItemsController < ApplicationController
   def create
+    list = List.find(params[:item][:list_id])
     @item = Item.new(item_params)
     if @item.valid?
       @item.save
       store_item(params[:item][:category_id],params[:item][:list_id],@item)
-      redirect_to lists_path
+      redirect_to category_path(list, list.next(@new_ulci.category.id))
     else
       flash[:error] = @user.errors.full_messages.join(". ")
       redirect_to user_path(current_user)
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,6 +1,6 @@
 module ItemsHelper
   def store_item(c,l,i)
-    new_ucli = UserListCategoryItem.create(category_id: c, list_id: l, item_id: i.id, user_id: current_user.id)
-    new_ucli.save
+    @new_ulci = UserListCategoryItem.create(category_id: c, list_id: l, item_id: i.id, user_id: current_user.id)
+    @new_ulci.save
   end
 end
