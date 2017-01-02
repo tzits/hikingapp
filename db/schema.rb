c50e958e2962f5b68af964b56ab207d8ed0b7f9c
tzits <tzitsman@gmail.com>
2017-01-01 16:44:46 -0800

    adds admin flag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128223659) do
+ActiveRecord::Schema.define(version: 20170102004334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 20161128223659) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.boolean  "admin"
   end
 
   add_foreign_key "checklist_items", "checklists"
