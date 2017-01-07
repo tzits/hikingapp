a27bb805bdb5dceb5597a3a4bde9f7bd783532af
tzits <tzitsman@gmail.com>
2017-01-06 19:07:53 -0800

    adds admin views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(first_name: 'toby', email: 'toby@toby.com', password_digest: 'password')
+User.create(first_name: 'admin', email: 'admin@example.com', password_digest: 'password')
 User.create(first_name: 'natalia', email: 'natalia@natalia.com', password_digest: 'password')
 User.create(first_name: 'teddy', email: 'teddy@teddy.com', password_digest: 'password')
 
