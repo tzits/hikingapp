30e0cf74cd2a3ac47eccf84a61a442f32c094307
tzits <tzitsman@gmail.com>
2017-01-17 14:08:30 -0800

    fixes checked box relocation glitch

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -1,6 +1,6 @@
 class ChecklistsController < ApplicationController
-  
+
   def show
-    @sql_list = User.find_by_sql("SELECT * FROM user_checklist_items WHERE user_id = #{current_user.id}")
+    @sql_list = User.find_by_sql("SELECT * FROM user_checklist_items WHERE user_id = #{current_user.id} ORDER BY checklist_item_id DESC")
   end
 end
