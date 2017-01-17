590570a88d3081270f70e5ca2bda1ffb62e01b97
tzits <tzitsman@gmail.com>
2017-01-17 11:07:39 -0800

    removes unnecessary sql commands

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,6 @@ class UsersController < ApplicationController
 
   def show
     @user =  User.find(params[:id])
-    @sql_hike = User.find_by_sql("SELECT * FROM user_list_category_items WHERE user_id = #{params[:id]} AND list_id = 1")
-    @sql_bike = User.find_by_sql("SELECT * FROM user_list_category_items WHERE user_id = #{params[:id]} AND list_id = 2")
-    @sql_canoe = User.find_by_sql("SELECT * FROM user_list_category_items WHERE user_id = #{params[:id]} AND list_id = 3")
   end
 
   def edit
